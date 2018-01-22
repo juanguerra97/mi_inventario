b7a7545c5386c557baf463d900632a545e7ebfb0
Juan Guerra <juanguerra97@protonmail.com>
2018-01-21 18:52:24 -0600

    Prueba de la clase Producto(metodo equals())

---
+++ src/test/java/inventario/modelo/TestProducto.java
@@ -0,0 +1,20 @@
+package inventario.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProducto {
+
+	@Test
+	public void testEquals() {
+		Producto p1 = new Producto(1,"NARANJAS","EL NARANJERO");
+		Producto p2 = new Producto(1,"IDREPETIDO","REP");
+		Producto p3 = new Producto(2,"NARANJAS","EL NARANJERO");
+		Producto p4 = new Producto(3,"LIMONES","EL LIMONERO");
+		assertEquals(p1, p2);
+		assertEquals(p1, p3);
+		assertNotEquals(p4, p1);
+	}
+
+}
