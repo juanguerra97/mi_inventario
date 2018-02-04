b5e5542bdf9b8fb2e3915e0ba0d553755c8b6ca8
Juan Guerra <juanguerra97@protonmail.com>
2018-02-04 08:54:36 -0600

    Clase con recursos utilizados en las clases del modelo de datos

---
+++ src/main/java/inventario/modelo/Resources.java
@@ -0,0 +1,15 @@
+package inventario.modelo;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Resources {
+	
+	/**
+	 * ResourceBundle con los strings utilizados en las clases del modelo de datos
+	 */
+	public static ResourceBundle STRINGS_MODELO = ResourceBundle.getBundle("strings/strings_modelo", Locale.getDefault());
+
+	private Resources() {}
+	
+}
