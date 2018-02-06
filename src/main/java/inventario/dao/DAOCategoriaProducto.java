2deefc40a091ecb4e9384d0e83a81040fbffc511
Juan Guerra <juanguerra97@protonmail.com>
2018-02-06 08:41:49 -0600

    Creacion de interfaz DAOCategoriaProducto con metodos para manipular la relacion entre productos y categorias

---
+++ src/main/java/inventario/dao/DAOCategoriaProducto.java
@@ -0,0 +1,60 @@
+package inventario.dao;
+
+import java.util.List;
+
+import inventario.dao.error.CannotInsertException;
+import inventario.dao.error.DBConnectionException;
+import inventario.modelo.Categoria;
+import inventario.modelo.Producto;
+
+public interface DAOCategoriaProducto {
+
+	/**
+	 * Metodo para obtener todos los productos de una categoria
+	 * @param categoria categoria a la que deben pertenecer los productos, no debe ser null
+	 * @param index posicion desde la que se deben obtener los registros
+	 * @param size numero maximo de registros a devolver
+	 * @return lista con todos los productos de la categoria
+	 */
+	List<Producto> getAll(String categoria, int index, int size);
+	
+	/**
+	 * Metodo para obtener todos los productos cuya categoria haga match con una expresion regular
+	 * @param index posicion desde la que se deben obtener los registros
+	 * @param size numero maximo de registros a devolver
+	 * @param regex expresion regular que se utilizara para comparar, no debe ser null
+	 * @return lista con todos los productos de las categorias que hagan match con regex
+	 */
+	List<Producto> getAll( int index, int size, String regex);
+	
+	/**
+	 * Metodo para obtener todas las categorias de un producto
+	 * @param idProducto ID del producto que debe estar asociado a las categorias
+	 * @param index posicion desde la que se deben recuperar los registros
+	 * @param size numero maximo de registros a devolver
+	 * @return lista con las categorias del producto
+	 */
+	List<Categoria> getAll(long idProducto, int index, int size);
+	
+	/**
+	 * Metodo para insertar una nueva relacion de producto y categoria
+	 * @param idProducto ID del producto
+	 * @param nomCategoria nombre de la categoria
+	 * @return true si la insercion se realiza con exito
+	 * @throws DBConnectionException si ocurre un error de conexion con la base de datos
+	 * @throws CannotInsertException si no se puede insertar la relacion, por ejemplo 
+	 * si ya existe la relacion
+	 */
+	boolean insert(long idProducto, String nomCategoria) throws DBConnectionException, 
+													CannotInsertException;
+	
+	/**
+	 * Metodo para eliminar una relacion de producto y categoria
+	 * @param idProducto ID del producto
+	 * @param nomCategoria nombre de la categoria
+	 * @return true si la relacion se elimina exitosamente
+	 * @throws DBConnectionException si ocurre un error de conexion con la base de datos
+	 */
+	boolean delete(long idProducto, String nomCategoria) throws DBConnectionException;
+	
+}
