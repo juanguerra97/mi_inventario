4756bc67c773f0adaa7cd453eb6933d9076e4227
Juan Guerra <juanguerra97@protonmail.com>
2018-02-26 08:24:53 -0600

    Modificacion de los tests de las clases de la jerarquia de items

--- src/test/java/inventario/modelo/TestItemCompra.java
+++ src/test/java/inventario/modelo/TestItemCompra.java
@@ -12,87 +12,87 @@ public class TestItemCompra {
 	@Test
 	public void testConstructor()
 throws ModelConstraintViolationException, EmptyArgumentException {
-		new ItemCompra(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemCompra(1, "BEBIDA", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = ModelConstraintViolationException.class)
 	public void testBadNumTransaccion() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(0, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemCompra(0, "BEBIDA", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
-	@Test(expected = ModelConstraintViolationException.class)
+	@Test(expected = EmptyArgumentException.class)
 	public void testBadIdProducto() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 0, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemCompra(1, "", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = EmptyArgumentException.class)
 	public void testBadMarca() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "", "BOTELLA 500ML", 1, 
+		new ItemCompra(1, "BEBIDA", "", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = EmptyArgumentException.class)
 	public void testBadPresentacion() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "MARCA", "", 1, 
+		new ItemCompra(1, "PRODUCTO", "MARCA", "", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = ModelConstraintViolationException.class)
 	public void testBadLote() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "MARCA", "PRESENTACION", 0, 
+		new ItemCompra(1, "PRODUCTO", "MARCA", "PRESENTACION", 0, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = ModelConstraintViolationException.class)
 	public void testBadCosto() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "MARCA", "PRESENTACION", 1, 
+		new ItemCompra(1, "PRODUCTO", "MARCA", "PRESENTACION", 1, 
 				new BigDecimal("-8.00"), 12, "MIGUEL");
 	}
 	
 	@Test(expected = ModelConstraintViolationException.class)
 	public void testBadCantidad() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "MARCA", "PRESENTACION", 1, 
+		new ItemCompra(1, "PRODUCTO", "MARCA", "PRESENTACION", 1, 
 				new BigDecimal("8.00"), 0, "MIGUEL");
 	}
 	
 	@Test(expected = EmptyArgumentException.class)
 	public void testBadProveedor() 
 			throws ModelConstraintViolationException, EmptyArgumentException  {
-		new ItemCompra(1, 1, "MARCA", "PRESENTACION", 1, 
+		new ItemCompra(1, "PRODUCTO", "MARCA", "PRESENTACION", 1, 
 				new BigDecimal("8.00"), 1, "");
 	}
 	
 	@Test
 	public void testEquals() 
 			throws ModelConstraintViolationException, EmptyArgumentException {
-		ItemCompra ic1 = new ItemCompra(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemCompra ic1 = new ItemCompra(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
-		ItemCompra ic2 = new ItemCompra(1, 1, "MARCA", "BOTELLA 500ML", 1, 
-				new BigDecimal("10.00"), 10, "JOSE");
+		ItemCompra ic2 = new ItemCompra(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
+				new BigDecimal("10.00"), 10, "MIGUEL");
 		assertEquals(ic1, ic2);
-		ItemCompra ic3 = new ItemCompra(2, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemCompra ic3 = new ItemCompra(2, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 		assertNotEquals(ic3, ic1);
-		ItemCompra ic4 = new ItemCompra(1, 2, "MARCA", "BOTELLA 500ML", 1, 
+		ItemCompra ic4 = new ItemCompra(1, "PRODUCTO2", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 		assertNotEquals(ic4, ic1);
-		ItemCompra ic5 = new ItemCompra(1, 1, "MARCA II", "BOTELLA 500ML", 1, 
+		ItemCompra ic5 = new ItemCompra(1, "PRODUCTO", "MARCA II", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 		assertNotEquals(ic5, ic1);
-		ItemCompra ic6 = new ItemCompra(1, 1, "MARCA", "LATA 500ML", 1, 
+		ItemCompra ic6 = new ItemCompra(1, "PRODUCTO", "MARCA", "LATA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 		assertNotEquals(ic6, ic1);
-		ItemCompra ic7 = new ItemCompra(1, 1, "MARCA II", "BOTELLA 500ML", 2, 
+		ItemCompra ic7 = new ItemCompra(1, "PRODUCTO", "MARCA II", "BOTELLA 500ML", 2, 
 				new BigDecimal("8.00"), 12, "MIGUEL");
 		assertNotEquals(ic7, ic1);
 	}
--- src/test/java/inventario/modelo/TestItemVenta.java
+++ src/test/java/inventario/modelo/TestItemVenta.java
@@ -16,76 +16,76 @@ public class TestItemVenta {
 	@Test
 	public void testConstructorI() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "1234567852345");
 	}
 	
 	@Test
 	public void testConstructorII() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				"Marcos", new BigDecimal("8.00"), 12);
 	}
 	
 	@Test
 	public void testConstructorIII() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				 new BigDecimal("8.00"), 12, "1234567852345", "Marcos");
 	}
 	
 	@Test(expected = ModelConstraintViolationException.class)
 	public void testConstructorIV() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				 new BigDecimal("8.00"), 12, null, null);
 	}
 	
 	@Test(expected = DataFormatException.class)
 	public void testBadDPI() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "1234567850345");
 	}
 
 	@Test(expected = EmptyArgumentException.class)
 	public void testBadDPI_II() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "");
 	}
 	
 	@Test(expected = EmptyArgumentException.class)
 	public void testBadNombre() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				"", new BigDecimal("8.00"), 12);
 	}
 	
 	@Test
 	public void testEquals() throws ModelConstraintViolationException, 
 	EmptyArgumentException, DataFormatException {
-		ItemVenta iv1 = new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv1 = new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("8.00"), 12, "1234567852345");
-		ItemVenta iv2 = new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv2 = new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				"Marcos", new BigDecimal("8.00"), 12);
 		assertEquals(iv1, iv2);
-		ItemVenta iv3 = new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv3 = new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertEquals(iv1, iv3);
-		ItemVenta iv4 = new ItemVenta(2, 1, "MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv4 = new ItemVenta(2, "PRODUCTO", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertNotEquals(iv4, iv1);
-		ItemVenta iv5 = new ItemVenta(1, 2, "MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv5 = new ItemVenta(1, "PRODUCTO2", "MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertNotEquals(iv5, iv1);
-		ItemVenta iv6 = new ItemVenta(1, 1, "OTRA MARCA", "BOTELLA 500ML", 1, 
+		ItemVenta iv6 = new ItemVenta(1, "PRODUCTO", "OTRA MARCA", "BOTELLA 500ML", 1, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertNotEquals(iv6, iv1);
-		ItemVenta iv7 = new ItemVenta(1, 1, "MARCA", "OTRA BOTELLA 500ML", 1, 
+		ItemVenta iv7 = new ItemVenta(1, "PRODUCTO", "MARCA", "OTRA BOTELLA 500ML", 1, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertNotEquals(iv7, iv1);
-		ItemVenta iv8 = new ItemVenta(1, 1, "MARCA", "BOTELLA 500ML", 5, 
+		ItemVenta iv8 = new ItemVenta(1, "PRODUCTO", "MARCA", "BOTELLA 500ML", 5, 
 				new BigDecimal("20.00"), 10, "1234567852345");
 		assertNotEquals(iv8, iv1);
 	}
