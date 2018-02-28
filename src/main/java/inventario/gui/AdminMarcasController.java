b4af7652d2c825ce24f92047717346ce2d75831e
Juan Guerra <juanguerra97@protonmail.com>
2018-02-28 08:51:22 -0600

    Implementacion de funcionalidad basica en el controlador de la interfaz de administracion de marcas

--- src/main/java/inventario/gui/AdminMarcasController.java
+++ src/main/java/inventario/gui/AdminMarcasController.java
@@ -1,11 +1,18 @@
 package inventario.gui;
 
-import inventario.modelo.Producto;
+import java.sql.SQLException;
+import inventario.dao.DAOMarca;
+import inventario.dao.error.DBConnectionException;
+import inventario.dao.mysql.Conexion;
+import inventario.dao.mysql.DAOMarcaMySQL;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import static inventario.gui.Main.ERROR;
+import static inventario.gui.Resources.STRINGS_GUI;
 
 /**
  * Controlador de eventos de la interfaz del archivo AdminMarcas.fxml
@@ -14,19 +21,61 @@ import javafx.scene.control.TextField;
  */
 public class AdminMarcasController {
 	
-	/**
-	 * ListView donde se muestran las marcas
-	 */
-	@FXML public ListView<String> listMarcas;
+	@FXML private ListView<String> listMarcas;
+	@FXML private TextField fieldNomMarca;
+	@FXML private Button btnNewMarca;
+	
+	private DAOMarca daoMarca;
+	private ObservableList<String> marcas;// lista con las marcas que se estan mostrando en el listview
+	
+	@FXML
+	private void initialize() {
+		
+		marcas = FXCollections.observableArrayList();
+		listMarcas.setItems(marcas);
+		
+		conectar();
+		if(daoMarca != null)
+			loadAll();
+		
+	}
 	
 	/**
-	 * Campo de texto para el nombre de una nueva marca
+	 * Metodo para activar o desactivar todos los componentes graficos
+	 * @param disable true si se deben desactivar los componentes
 	 */
-	@FXML public TextField fieldNomMarca;
+	public void disableAll(boolean disable) {
+		listMarcas.setDisable(disable);
+		fieldNomMarca.setDisable(disable);
+		btnNewMarca.setDisable(disable);
+	}
 	
 	/**
-	 * Boton para guardar una nueva marca con el nombre del campo de texto
+	 * Metodo para realizar la conexion con la base de datos si fallo al principio
 	 */
-	@FXML public Button btnNewMarca;
-
+	public void conectar() {
+		if(daoMarca != null)
+			return;
+		try {
+			daoMarca = new DAOMarcaMySQL(Conexion.get());
+			disableAll(false);
+		} catch (DBConnectionException | SQLException e) {
+			disableAll(true);
+			ERROR.setHeaderText(STRINGS_GUI.getString("error.conexion.header"));
+			ERROR.setContentText(e.getMessage());
+			ERROR.showAndWait();
+		}
+	}
+	
+	/**
+	 * Metodo para cargar todas las marcas en la lista de marcas
+	 */
+	public void loadAll() {
+		assert daoMarca != null;
+		listMarcas.getSelectionModel().clearSelection();
+		marcas.setAll(daoMarca.getAll(0, 40));
+		boolean isEmpty = marcas.isEmpty();
+		listMarcas.setDisable(isEmpty);
+	}
+	
 }
