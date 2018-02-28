36eb90fcc3ccb8d48588f682c176b0dc0b2abac9
Juan Guerra <juanguerra97@protonmail.com>
2018-02-28 08:19:51 -0600

    Clase con recursos utilizados en las clases de la interfaz grafica

---
+++ src/main/java/inventario/gui/Resources.java
@@ -0,0 +1,15 @@
+package inventario.gui;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Resources {
+	
+	/**
+	 * ResourceBundle con los strings utilizados en la interfaz grafica
+	 */
+	public static final ResourceBundle STRINGS_GUI = ResourceBundle.getBundle("strings/strings_gui", Locale.getDefault());
+	
+	private Resources() {}
+
+}
