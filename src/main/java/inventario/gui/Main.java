403d10317efac95b5265e8f45eddc571238c998b
Juan Guerra <juanguerra97@protonmail.com>
2018-03-02 08:40:25 -0600

    Se carga la interfaz de inicio en la ventana principal

--- src/main/java/inventario/gui/Main.java
+++ src/main/java/inventario/gui/Main.java
@@ -1,11 +1,17 @@
 package inventario.gui;
 
+import static inventario.gui.Resources.STRINGS_GUI;
+
+import org.controlsfx.control.NotificationPane;
+
+import inventario.dao.mysql.Conexion;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
-import static inventario.gui.Resources.STRINGS_GUI;
 
 /**
  * Clase con metodo principal para iniciar la aplicacion
@@ -24,9 +30,21 @@ public class Main extends Application {
 	 */
 	public static Alert INFO = new Alert(AlertType.INFORMATION,"",new ButtonType(STRINGS_GUI.getString("entendido")));
 	
+	private static NotificationPane root = new NotificationPane();
+	
 	@Override
 	public void start(Stage stg) throws Exception {
-		
+		root.setContent(FXMLLoader.load(
+				getClass().getResource("/inventario/gui/HomeGUI.fxml"), 
+				Resources.STRINGS_GUI));
+		Scene s = new Scene(root,750,450);
+		stg.setScene(s);
+		stg.setMinWidth(750);
+		stg.setMinHeight(450);
+		stg.setOnCloseRequest(w->{
+			Conexion.close();
+		});
+		stg.show();
 	}
 
 	public static void main(String[] args) {
