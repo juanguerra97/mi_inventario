8de98a7412c1e3af638cc8782f15dfadcfa0d8ac
Juan Guerra <juanguerra97@protonmail.com>
2018-03-09 08:28:51 -0600

    Metodos relacionados con scroll

---
+++ src/main/java/inventario/gui/Scrolls.java
@@ -0,0 +1,48 @@
+package inventario.gui;
+
+import javafx.geometry.Orientation;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollBar;
+
+public class Scrolls {
+
+	/**
+	 * Metodo para obtener el scroll bar vertical de un listview
+	 * @param listView lista de la que se obtendra el scrollbar, no debe ser null
+	 * @return scroll bar si la lista lo tiene, null si no se puede obtener
+	 */
+	public static ScrollBar getListViewVerticalScrollBar(ListView<?> listView) {
+		ScrollBar scrollbar = null;
+		for (Node node : listView.lookupAll(".scroll-bar")) {
+			if (node instanceof ScrollBar) {
+				ScrollBar bar = (ScrollBar) node;
+				if (bar.getOrientation().equals(Orientation.VERTICAL)) {
+					scrollbar = bar;
+					break;
+				}
+			}
+		}
+		return scrollbar;
+	}
+	
+	/**
+	 * Metodo para obtener el scroll bar vertical de un tableview
+	 * @param tableView tabla de la que se obtendra el scrollbar, no debe ser null
+	 * @return scroll bar si la tabla lo tiene, null si no se puede obtener
+	 */
+	public static ScrollBar getTableViewVerticalScrollBar(ListView<?> tableView) {
+		ScrollBar scrollbar = null;
+		for (Node node : tableView.lookupAll(".scroll-bar")) {
+			if (node instanceof ScrollBar) {
+				ScrollBar bar = (ScrollBar) node;
+				if (bar.getOrientation().equals(Orientation.VERTICAL)) {
+					scrollbar = bar;
+					break;
+				}
+			}
+		}
+		return scrollbar;
+	}
+	
+}
