362b40039a4a9a0efc965835168d49f15a2b5420
Juan Guerra <juanguerra97@protonmail.com>
2018-03-11 10:33:55 -0600

    Se crearon metodos para cargar marcas y productos en su list y table view, ademas de eventos de cargar datos onScoll

--- src/main/java/inventario/gui/AdminInventarioController.java
+++ src/main/java/inventario/gui/AdminInventarioController.java
@@ -4,14 +4,28 @@ import static inventario.gui.Resources.STRINGS_GUI;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.controlsfx.control.NotificationPane;
 
+import inventario.dao.DAOCategoriaProducto;
+import inventario.dao.DAOMarca;
+import inventario.dao.DAOProducto;
+import inventario.dao.error.DBConnectionException;
+import inventario.dao.mysql.Conexion;
+import inventario.dao.mysql.DAOCategoriaProductoMySQL;
+import inventario.dao.mysql.DAOMarcaMySQL;
+import inventario.dao.mysql.DAOProductoMySQL;
 import inventario.modelo.Lote;
 import inventario.modelo.Presentacion;
 import inventario.modelo.PresentacionLote;
 import inventario.modelo.Producto;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -27,6 +41,8 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class AdminInventarioController {
+	
+	private static final int MAX = 500, STEP = 10;
 	
 	@FXML private ListView<String> listMarcas;
 	@FXML private MenuItem menuItemDesMarca;
@@ -68,10 +84,22 @@ public class AdminInventarioController {
 	private NotificationPane paneNewProd;
 	private NewProductoController newProdCtrl;
 	
+	private DAOProducto daoProds = null;
+	private DAOCategoriaProducto daoCatsProd = null;
+	private DAOMarca daoMarcas = null;
+	
 	FiltroProd filtroProductos = FiltroProd.NONE;
 	
 	@FXML
 	private void initialize() {
+		
+		try {
+			daoProds = new DAOProductoMySQL(Conexion.get());
+			daoCatsProd = new DAOCategoriaProductoMySQL(Conexion.get());
+			daoMarcas = new DAOMarcaMySQL(Conexion.get());
+		} catch (DBConnectionException | SQLException ex) {
+			ex.printStackTrace();
+		}
 		
 		colIdProd.setCellValueFactory(new PropertyValueFactory<>("id"));
 		colNomProd.setCellValueFactory(new PropertyValueFactory<>("nombre"));
@@ -87,6 +115,24 @@ public class AdminInventarioController {
 		colPrecInv.setCellValueFactory(new PropertyValueFactory<>("precio"));
 		colCostInv.setCellValueFactory(new PropertyValueFactory<>("costo"));
 		colCantInv.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
+	
+		ExecutorService executor = Executors.newCachedThreadPool();
+		executor.execute(()->{
+			// evento para cargar datos al hacer scroll 
+			// se hace en otro hilo porque la tabla tiene que estar renderizada para poder
+			// obtener su ScrollBar
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			LoadData.<Producto>loadOnScroll(tblProds, 
+					()->getProdDataScroll());
+			LoadData.<String>loadOnScroll(listMarcas, 
+					()->daoMarcas.getAll(listMarcas.getItems().size(), 
+				    STEP));
+		});
+		executor.shutdown();
 		
 		vtnNewProd = new Stage();
 		vtnNewProd.setMinWidth(300);
@@ -94,6 +140,7 @@ public class AdminInventarioController {
 		vtnNewProd.initModality(Modality.APPLICATION_MODAL);
 		vtnNewProd.setTitle(STRINGS_GUI.getString("title.new.producto"));
 		paneNewProd = new NotificationPane();
+		
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/inventario/gui/NewProducto.fxml"),STRINGS_GUI);
 		try {
 			paneNewProd.setContent(loader.load());
@@ -104,6 +151,65 @@ public class AdminInventarioController {
 			e.printStackTrace();
 		}
 		
+		cargarMarcas();
+		cargarProductos();
+		
+	}
+	
+	private void cargarProductos() {
+		boolean mar = checkFiltrarProdsMarca.isSelected();
+		boolean cat = checkFiltrarProdsCategoria.isSelected();
+		Producto p = tblProds.getSelectionModel().getSelectedItem();
+		tblProds.getSelectionModel().clearSelection();
+		ObservableList<Producto> items = tblProds.getItems();
+		if(mar && cat) {
+			
+		} else if(mar) {
+			items.setAll(daoProds.getAll(
+						listMarcas.getSelectionModel().getSelectedItem(), 
+						0, 30));
+		} else if(cat) {
+			items.setAll(daoCatsProd.getAll(
+						listCategorias.getSelectionModel().getSelectedItem(), 
+						0, 30));
+		} else {
+			items.setAll(daoProds.getAll(0, 50));
+		}
+		tblProds.getSelectionModel().select(p);
+	}
+	
+	private void cargarMarcas() {
+		String m = listMarcas.getSelectionModel().getSelectedItem();
+		listMarcas.getSelectionModel().clearSelection();
+		listMarcas.getItems().setAll(daoMarcas.getAll(0, 50));
+		listMarcas.getSelectionModel().select(m);
+	}
+	
+	private List<Producto> getProdDataScroll() {
+		List<Producto> prods = new LinkedList<>();
+		List<Producto> items = tblProds.getItems();
+		if(items.size() <= (MAX - STEP) && daoProds != null) {
+			int index = items.size();
+			switch(filtroProductos) {
+			case MARCA:
+				prods = daoProds.getAll(
+						listMarcas.getSelectionModel().getSelectedItem(), 
+						index, STEP);
+				break;
+			case CATEGORIA:
+				prods = daoCatsProd.getAll(
+						listCategorias.getSelectionModel().getSelectedItem(), 
+						index, STEP);
+				break;
+			case MARCA_CATEGORIA:
+				break;
+			case NONE:
+			default:
+				prods = daoProds.getAll(index, STEP);
+				break;
+			}
+		}
+		return prods;
 	}
 	
 	
@@ -112,6 +218,8 @@ public class AdminInventarioController {
 		newProdCtrl.reset();
 		vtnNewProd.centerOnScreen();
 		vtnNewProd.showAndWait();
+		cargarMarcas();
+		cargarProductos();
 	}
 	
 	@FXML
