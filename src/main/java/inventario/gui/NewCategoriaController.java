e8163b775dc71b8055ee2e9e0446d885ed2b92b0
Juan Guerra <juanguerra97@protonmail.com>
2018-03-11 16:24:28 -0600

    Creacion de clase controladora de eventos de la interfaz NewCategoria.fxml

---
+++ src/main/java/inventario/gui/NewCategoriaController.java
@@ -0,0 +1,17 @@
+package inventario.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewCategoriaController {
+	
+	@FXML TextField fieldNombre;
+	@FXML Button btnGuardar;
+	
+	@FXML
+	private void initialize() {
+		
+	}
+
+}
