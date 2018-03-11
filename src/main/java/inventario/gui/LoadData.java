c289733e16d8832c693ce54de48cb2945a7f17fe
Juan Guerra <juanguerra97@protonmail.com>
2018-03-11 09:59:26 -0600

    Se quito el parametro max de los metodos loadOnScroll...

--- src/main/java/inventario/gui/LoadData.java
+++ src/main/java/inventario/gui/LoadData.java
@@ -18,23 +18,17 @@ public class LoadData {
 	 * Metodo para agregar evento a una tabla para que se cargen mas datos al hacer scroll
 	 * @param table tabla a la que se agregara el evento, no puede ser null
 	 * @param supplier metodo que proporciona los datos, no debe ser null
-	 * @param max numero maximo de elementos que puede tener la tabla
 	 */
-	public static <E> void loadOnScroll(TableView<E> table, Supplier<List<E>> supplier, int max) {
+	public static <E> void loadOnScroll(TableView<E> table, Supplier<List<E>> supplier) {
 		assert table != null;
 		assert supplier != null;
-		if(max <= 0)
-			max = DEFAULT_MAX;
-		final int MAX = max;
 		ScrollBar scrollBar = Scrolls.getTableViewVerticalScrollBar(table);
 		assert scrollBar != null : STRINGS_GUI.getString("error.scrollbar.null");
 		scrollBar.valueProperty().addListener((observable, oldValue, newValue) -> {
 	        double position = newValue.doubleValue();
 	        if (position >= scrollBar.getMax() - 10) {
-	        	int numItems = table.getItems().size();
 	        	List<E> newItems = supplier.get();
-	            if (numItems + newItems.size() <= MAX)
-	            	table.getItems().addAll(newItems);
+	            table.getItems().addAll(newItems);
 	        }
 	    });
 	}
@@ -43,23 +37,17 @@ public class LoadData {
 	 * Metodo para agregar evento a una lista para que se cargen mas datos al hacer scroll 
 	 * @param list lista a la que se agregara el evento, no puede ser null
 	 * @param supplier metodo que proporciona los datos, no debe ser null
-	 * @param max numero maximo de elementos que puede tener la lista
 	 */
-	public static <E> void loadOnScroll(ListView<E> list, Supplier<List<E>> supplier, int max) {
+	public static <E> void loadOnScroll(ListView<E> list, Supplier<List<E>> supplier) {
 		assert list != null;
 		assert supplier != null;
-		if(max <= 0)
-			max = DEFAULT_MAX;
-		final int MAX = max;
 		ScrollBar scrollBar = Scrolls.getListViewVerticalScrollBar(list);
 		assert scrollBar != null : STRINGS_GUI.getString("error.scrollbar.null");
 		scrollBar.valueProperty().addListener((observable, oldValue, newValue) -> {
 	        double position = newValue.doubleValue();
 	        if (position >= scrollBar.getMax() - 10) {
-	        	int numItems = list.getItems().size();
 	        	List<E> newItems = supplier.get();
-	            if (numItems + newItems.size() <= MAX)
-	            	list.getItems().addAll(newItems);
+	            list.getItems().addAll(newItems);
 	        }
 	    });
 	}
