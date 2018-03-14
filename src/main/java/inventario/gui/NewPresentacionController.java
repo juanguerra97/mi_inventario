e8064cc3a2943a3292ba800ef23fa07cf8f9afcc
Juan Guerra <juanguerra97@protonmail.com>
2018-03-14 08:05:39 -0600

    Interfaz y controller para ingresar una nueva presentacion

---
+++ src/main/java/inventario/gui/NewPresentacionController.java
@@ -0,0 +1,18 @@
+package inventario.gui;
+
+import org.controlsfx.control.textfield.CustomTextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class NewPresentacionController {
+	
+	@FXML private CustomTextField fieldNombre;
+	@FXML private Button btnGuardar;
+	
+	@FXML
+	private void initialize() {
+		
+	}
+
+}
