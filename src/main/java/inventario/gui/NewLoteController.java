9db39dd275333677fb6997b9ae1d29e455b1198c
Juan Guerra <juanguerra97@protonmail.com>
2018-03-15 08:16:26 -0600

    Creacion del controlador de eventos de la interfaz NewLote.fxml

---
+++ src/main/java/inventario/gui/NewLoteController.java
@@ -0,0 +1,26 @@
+package inventario.gui;
+
+import org.controlsfx.control.textfield.CustomTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+
+public class NewLoteController {
+	
+	@FXML private CustomTextField fieldNumero;
+	@FXML private DatePicker datePicker;
+	@FXML private Button btnGuardar;
+	
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	@FXML
+	private void onGuardar(ActionEvent event) {
+		
+	}
+
+}
